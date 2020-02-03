ad2054084c4bd70c28f969b99f398303cafa29ac
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-03 19:13:13 +0530

    add material bottom tab navigator

--- navigation/MealsNavigator.jsx
+++ navigation/MealsNavigator.jsx
@@ -11,6 +11,7 @@ import Colors from '../constants/Colors';
 import { Platform } from 'react-native';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import { Ionicons } from '@expo/vector-icons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 const MealsNavigator = createStackNavigator(
 	{
@@ -35,38 +36,42 @@ const MealsNavigator = createStackNavigator(
 	}
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator(
-	{
-		Meals: {
-			screen: MealsNavigator,
-			navigationOptions: {
-				tabBarIcon: tabInfo => {
-					return (
-						<Ionicons
-							name="ios-restaurant"
-							size={25}
-							color={tabInfo.tintColor}
-						/>
-					);
-				}
-			}
-		},
-		Favorites: {
-			screen: FavoritesScreen,
-			navigationOptions: {
-				tabBarIcon: tabInfo => {
-					return (
-						<Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
-					);
-				}
-			}
+const tabScreenConfig = {
+	Meals: {
+		screen: MealsNavigator,
+		navigationOptions: {
+			tabBarIcon: tabInfo => {
+				return (
+					<Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+				);
+			},
+			tabBarColor: Colors.primaryColor
 		}
 	},
-	{
-		tabBarOptions: {
-			activeTintColor: Colors.accentColor
+	Favorites: {
+		screen: FavoritesScreen,
+		navigationOptions: {
+			tabBarIcon: tabInfo => {
+				return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+			},
+			tabBarColor: Colors.accentColor
 		}
 	}
-);
+};
+
+const MealsFavTabNavigator =
+	Platform.OS === 'android'
+		? createMaterialBottomTabNavigator(tabScreenConfig, {
+				activeTintColor: 'white',
+				shifting: true,
+				barStyle: {
+					backgroundColor: Colors.primaryColor
+				}
+		  })
+		: createBottomTabNavigator(tabScreenConfig, {
+				tabBarOptions: {
+					activeTintColor: Colors.accentColor
+				}
+		  });
 
 export default createAppContainer(MealsFavTabNavigator);
