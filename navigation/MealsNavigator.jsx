274a917ff5be08ca57f3ab1ffe93d93315a62a40
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-04 11:20:34 +0530

    add favorite screen meal item

--- navigation/MealsNavigator.jsx
+++ navigation/MealsNavigator.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  createStackNavigator,
-  createAppContainer,
-  createBottomTabNavigator
+	createStackNavigator,
+	createAppContainer,
+	createBottomTabNavigator
 } from 'react-navigation';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
@@ -14,77 +14,77 @@ import { Ionicons } from '@expo/vector-icons';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 const defaultStackNavOptions = {
-  headerStyle: {
-    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-  },
-  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
-  headerTitle: 'A Screen'
+	headerStyle: {
+		backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+	},
+	headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+	headerTitle: 'A Screen'
 };
 
 const MealsNavigator = createStackNavigator(
-  {
-    Categories: {
-      screen: CategoriesScreen,
-      navigationOptions: {
-        headerTitle: 'Meal Categories'
-      }
-    },
-    CategoryMeals: {
-      screen: CategoryMealsScreen
-    },
-    MealDetail: { screen: MealDetailsScreen }
-  },
-  {
-    defaultNavigationOptions: defaultStackNavOptions
-  }
+	{
+		Categories: {
+			screen: CategoriesScreen,
+			navigationOptions: {
+				headerTitle: 'Meal Categories'
+			}
+		},
+		CategoryMeals: {
+			screen: CategoryMealsScreen
+		},
+		MealDetail: { screen: MealDetailsScreen }
+	},
+	{
+		defaultNavigationOptions: defaultStackNavOptions
+	}
 );
 
 const FavNavigator = createStackNavigator(
-  {
-    Favorites: FavoritesScreen,
-    MealDetail: MealDetailsScreen
-  },
-  {
-    defaultNavigationOptions: defaultStackNavOptions
-  }
+	{
+		Favorites: FavoritesScreen,
+		MealDetail: MealDetailsScreen
+	},
+	{
+		defaultNavigationOptions: defaultStackNavOptions
+	}
 );
 
 const tabScreenConfig = {
-  Meals: {
-    screen: MealsNavigator,
-    navigationOptions: {
-      tabBarIcon: tabInfo => {
-        return (
-          <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
-        );
-      },
-      tabBarColor: Colors.primaryColor
-    }
-  },
-  Favorites: {
-    screen: FavNavigator,
-    navigationOptions: {
-      tabBarIcon: tabInfo => {
-        return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
-      },
-      tabBarColor: Colors.accentColor
-    }
-  }
+	Meals: {
+		screen: MealsNavigator,
+		navigationOptions: {
+			tabBarIcon: tabInfo => {
+				return (
+					<Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+				);
+			},
+			tabBarColor: Colors.primaryColor
+		}
+	},
+	Favorites: {
+		screen: FavNavigator,
+		navigationOptions: {
+			tabBarIcon: tabInfo => {
+				return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+			},
+			tabBarColor: Colors.accentColor
+		}
+	}
 };
 
 const MealsFavTabNavigator =
-  Platform.OS === 'android'
-    ? createMaterialBottomTabNavigator(tabScreenConfig, {
-        activeTintColor: 'white',
-        shifting: true,
-        barStyle: {
-          backgroundColor: Colors.primaryColor
-        }
-      })
-    : createBottomTabNavigator(tabScreenConfig, {
-        tabBarOptions: {
-          activeTintColor: Colors.accentColor
-        }
-      });
+	Platform.OS === 'android'
+		? createMaterialBottomTabNavigator(tabScreenConfig, {
+				activeTintColor: 'white',
+				shifting: true,
+				barStyle: {
+					backgroundColor: Colors.primaryColor
+				}
+		  })
+		: createBottomTabNavigator(tabScreenConfig, {
+				tabBarOptions: {
+					activeTintColor: Colors.accentColor
+				}
+		  });
 
 export default createAppContainer(MealsFavTabNavigator);
--- screens/FavoritesScreen.jsx
+++ screens/FavoritesScreen.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import MealList from '../components/MealList';
+import { MEALS } from '../data/dummy-data';
 
-const FavoritesScreen = () => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Favorites Screen!</Text>
-    </View>
-  );
+const FavoritesScreen = props => {
+	const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+
+	return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
 FavoritesScreen.navigationOptions = {
-  headerTitle: 'Your Favorites'
+	headerTitle: 'Your Favorites'
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
 
 export default FavoritesScreen;
