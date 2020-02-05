35d372ef3dcc4ea45c21dbb09469d1a359bd37d4
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-02-05 12:12:16 +0530

    add redux

--- App.js
+++ App.js
@@ -4,8 +4,17 @@ import { useScreens } from 'react-native-screens';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import MealsNavigator from './navigation/MealsNavigator';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import mealsReducer from './store/reducers/meals';
 
 useScreens();
+
+const rootReducer = combineReducers({
+	meals: mealsReducer
+});
+
+const store = createStore();
 
 const fetchFonts = () => {
 	return Font.loadAsync({
@@ -23,7 +32,11 @@ export default function App() {
 		);
 	}
 
-	return <MealsNavigator />;
+	return (
+		<Provider store={store}>
+			<MealsNavigator />
+		</Provider>
+	);
 }
 
 const styles = StyleSheet.create({});
